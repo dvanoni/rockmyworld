ed2e7715d9e52d0afd443e63849dfb4b8aa2e5b2
rockmyworld <rockmyworld@53cd88d6-a311-47d8-bf01-29af0bf8c0a0>
2010-04-09 19:46:57 +0000

    pimped out loading screen

--- flickr.php
+++ flickr.php
@@ -1,10 +1,11 @@
 <?php
 $query 	 = urlencode($_REQUEST['query']);
 $queries = preg_split( '/,/', $query );
+$artist = $queries[0];
 
 $url = "http://query.yahooapis.com/v1/public/yql?q=".
 		"select%20source%20from%20flickr.photos.sizes%20where%20photo_id%20in%20".
-		"(select%20id%20from%20flickr.photos.search%20where%20text%3D%22$queries[0]%22)&format=json";
+		"(select%20id%20from%20flickr.photos.search%20where%20text%3D%22$artist%22)&format=json";
 
 $obj = json_decode( file_get_contents( $url ) );
 
