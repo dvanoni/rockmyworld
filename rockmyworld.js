89300dbcfa2024c3eaa85fd9129adfe7f131caa2
rockmyworld <rockmyworld@53cd88d6-a311-47d8-bf01-29af0bf8c0a0>
2010-04-09 11:06:31 +0000

    side scrolling test

--- rockmyworld.js
+++ rockmyworld.js
@@ -27,6 +27,7 @@ function handler( loc ) {
 						"<div class='date'>" + d.getMonth() + "/" + d.getDate() + "</div>" +
 						"<span class='event-title'>" + artist + "</span>" +
 					"</div>" +
+					"<div id='event" + i + "' style='display:none;'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>" +
 				"</div>";
 		}
 		
@@ -63,7 +64,7 @@ function handler( loc ) {
 }
 
 function openEvent( eventId ) {
-	$("#results").hide("slide", { direction: "left" }, 2000);
+	$( '#event' + eventId ).slideDown();
 }
 
 function loadURL( divId, URL ) {
