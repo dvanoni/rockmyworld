3736e8e1c96ab72d84b9463955a99b514bf2a926
rockmyworld <rockmyworld@53cd88d6-a311-47d8-bf01-29af0bf8c0a0>
2010-04-09 12:02:10 +0000

    event info

--- rockmyworld.js
+++ rockmyworld.js
@@ -29,7 +29,7 @@ function handler( loc ) {
 					"</div>" +
 				"</div>" + 
 				"<div id='event" + i + "' class='event-info'>" + 
-					"<div>" + events[i].artist + "</div>"
+					"<div style='margin:-4px 0 8px 0;'>" + events[i].artist + "</div>"
 					"<div style='padding:0 8px 8px 8px;'>";
 					
 			if( events[i].description )
