7614ee3e6929cafecdfe964fb2bce9e876a05be8
rockmyworld <rockmyworld@53cd88d6-a311-47d8-bf01-29af0bf8c0a0>
2010-04-09 11:21:26 +0000

    event info

--- rockmyworld.js
+++ rockmyworld.js
@@ -28,7 +28,17 @@ function handler( loc ) {
 						"<span class='event-title'>" + artist + "</span>" +
 					"</div>" +
 				"</div>" + 
-				"<div id='event" + i + "' class='event-info'><div style='padding:0 8px 8px 8px;'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div></div>";
+				"<div id='event" + i + "' class='event-info'>" + 
+					"<div style='padding:0 8px 8px 8px;'>";
+					
+			if( events[i].venueLocation.point ) {
+				html += "<div>" + events[i].venueName + "</div>" + 
+						"<div>" + events[i].venueLocation.street + "</div>" + 
+						"<div>" + events[i].venueLocation.city + ", " + events[i].venueLocation.postalcode + " </div>";
+			}
+			
+			html += "</div>" + 
+				"</div>";
 		}
 		
 		// Append HTML to the page
